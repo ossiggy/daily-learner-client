dac2a881eab3a923fea51116d645ca1a087eb0c2
Ossiggy <mrossig201@gmail.com>
2017-11-30 23:01:03 -0500

    all 4 endpoint actions added

--- src/actions/article.js
+++ src/actions/article.js
@@ -31,6 +31,23 @@ export const fetchArticle = () => dispatch => {
   });
 };
 
+export const POST_ARTICLE_SUCCESS = 'POST_ARTICLE_SUCCESS';
+export const postArticleSuccess = article => ({
+  type: POST_ARTICLE_SUCCESS,
+  article
+});
+
+export const POST_ARTICLE_REQUEST ='POST_ARTICLE_REQUEST';
+export const postArticleRequest = () => ({
+  type: POST_ARTICLE_REQUEST
+});
+
+export const POST_ARTICLE_ERROR = 'POST_ARTICLE_ERROR';
+export const postArticleError =(err) => ({
+  type: POST_ARTICLE_ERROR,
+  error: err
+})
+
 export const postArticle = article => dispatch => {
     return fetch(`${API_BASE_URL}/articles`, {
         method: 'POST',
@@ -51,4 +68,69 @@ export const postArticle = article => dispatch => {
                 );
             }
         });
+};
+
+export const UPDATE_ARTICLE_SUCCESS = 'UPDATE_ARTICLE_SUCCESS';
+export const updateArticleSuccess = article => ({
+  type: UPDATE_ARTICLE_SUCCESS,
+  article
+});
+
+export const UPDATE_ARTICLE_REQUEST ='UPDATE_ARTICLE_REQUEST';
+export const updateArticleRequest = () => ({
+  type: UPDATE_ARTICLE_REQUEST
+});
+
+export const UPDATE_ARTICLE_ERROR = 'UPDATE_ARTICLE_ERROR';
+export const updateArticleError =(err) => ({
+  type: UPDATE_ARTICLE_ERROR,
+  error: err
+})
+
+export const updateArticle = article => dispatch => {
+  return fetch(`${API_BASE_URL}/articles`, {
+      method: 'PUT',
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: Object.assign({}, res.json(), JSON.stringify(article))
+  })
+      .then(res => normalizeResponseErrors(res))
+      .then(res => res.json())
+      .catch(err => {
+          const {reason, message, location} = err;
+          if (reason === 'ValidationError') {
+              return Promise.reject(
+                  new SubmissionError({
+                      [location]: message
+                  })
+              );
+          }
+      });
+};
+
+export const DELETE_ARTICLE_SUCCESS = 'DELETE_ARTICLE_SUCCESS';
+export const deleteArticleSuccess = article => ({
+  type: DELETE_ARTICLE_SUCCESS,
+  article
+});
+
+export const DELETE_ARTICLE_REQUEST ='DELETE_ARTICLE_REQUEST';
+export const deleteArticleRequest = () => ({
+  type: DELETE_ARTICLE_REQUEST
+});
+
+export const DELETE_ARTICLE_ERROR = 'DELETE_ARTICLE_ERROR';
+export const deleteArticleError =(err) => ({
+  type: DELETE_ARTICLE_ERROR,
+  error: err
+})
+
+export const DELETE_ARTICLE = 'DELETE_ARTICLE';
+export const deleteArticle = () => dispatch => {
+  return fetch(`${REACT_APP_API_BASE_URL}/api/articles/:id`, {
+    method: 'delete'
+  }).then(() => {
+    dispatch(fetchDog())
+  });
 };
