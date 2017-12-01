d21f063557b2ed23343dfde25ea49b79481d2ceb
Ossiggy <mrossig201@gmail.com>
2017-11-30 22:22:41 -0500

    updated server to include all endpoints, created schemas for user and articles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
-import App from './App';
+import Dashboard from './Dashboard';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const posts = [{
+  title: 'hello',
+  content: 'hello hello hello',
+  dateCreated: '28 November 2017',
+  tags: ['hi', 'hey']
+},
+{
+  title: 'goodbye',
+  content: 'bye bye bye',
+  dateCreated: '28 November 2017',
+  tags: ['later', 'bye']  
+}]
+
+ReactDOM.render(<Dashboard posts={posts} />, document.getElementById('root'));
 registerServiceWorker();
