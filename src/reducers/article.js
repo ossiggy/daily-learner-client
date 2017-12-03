d470040b9dab38529d5c057d6e9020006fee706c
Ossiggy <mrossig201@gmail.com>
2017-12-02 21:20:12 -0500

    all reducers added

--- src/reducers/article.js
+++ src/reducers/article.js
@@ -1,2 +1,98 @@
 import * as actions from '../actions/article';
 
+const initialState = {
+  data: null,
+  loading: false,
+  error: null
+};
+
+export const articleReducer = (state=initialState, action) => {
+  if(action.type === actions.FETCH_ARTICLES_SUCCESS) {
+    return Object.assign({}, state, {
+      data: action.articles, //I am expecting an array to be returned from this action
+      loading: false
+    })
+  }
+  if(action.type === actions.FETCH_ARTICLES_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  }
+  if(action.type === actions.FETCH_ARTICLES_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error,
+      loading: false
+    })
+  }
+
+  if(action.type === actions.FETCH_ARTICLE_SUCCESS) {
+    return Object.assign({}, state, {
+      data: action.article,
+      loading: false
+    })
+  }
+  if(action.type === actions.FETCH_ARTICLE_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  }
+  if(action.type === actions.FETCH_ARTICLE_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error,
+      loading: true
+    })
+  }
+
+  if(action.type === actions.POST_ARTICLE_SUCCESS) {
+    return Object.assign({}, state, {
+      data: action.article,
+      loading: false
+    })
+  }
+  if(action.type === actions.POST_ARTICLE_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  }
+  if(action.type === actions.POST_ARTICLE_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error,
+      loading: false
+    })
+  }
+
+  if(action.type === actions.UPDATE_ARTICLE_SUCCESS) {
+    return Object.assign({}, state, {
+      data: action.article
+    })
+  }
+  if(action.type === actions.UPDATE_ARTICLE_REQUEST) {
+    return Onject.assign({}, state, {
+      loading: true
+    })
+  }
+  if(action.type === actions.UPDATE_ARTICLE_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error,
+      loading: false
+    })
+  }
+
+  if(action.type === actions.DELETE_ARTICLE_SUCCESS) {
+    return Object.assign({}, state, {
+      data: null,
+      loading: false
+    })
+  }
+  if(action.type === actions.DELETE_ARTICLE_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  }
+  if(action.type === actions.DELETE_ARTICLE_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error,
+      loading: false
+    })
+  }
+}
