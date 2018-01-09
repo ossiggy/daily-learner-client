5341cdc0466d6237927c2be78b4f2b5a16af95e2
Ossiggy <mrossig201@gmail.com>
2018-01-08 19:59:51 -0500

    continuous style updating

--- src/actions/singleArticle.js
+++ src/actions/singleArticle.js
@@ -94,7 +94,6 @@ export const updateArticleError =(err) => ({
 });
 
 export const updateArticle = article => (dispatch, getState) => {
-  console.log(JSON.stringify(article));
   const authToken = getState().auth.authToken;
   return fetch(`${API_BASE_URL}/articles/${article.id}`, {
     method: 'PUT',
