c0143a9fba1359d59947f8fd9d1c664dd6720527
Ossiggy <mrossig201@gmail.com>
2018-01-12 16:16:12 -0500

    article updater test failing, deleteArticle test failing, all others passing

--- src/__tests__/article-updater.js
+++ src/__tests__/article-updater.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
+import * as actions from '../actions';
 
 import {ArticleUpdater} from '../components/article-updater';
 
 describe('<ArticleUpdater />', () => {
   it('Renders without crashing', () => {
     shallow(<ArticleUpdater />);
+  });
+  it('Dispatches fetchArticle when mounted', () => {
+    const id = 12345;
+    const dispatch = jest.fn();
+    const wrapper = mount(<ArticleUpdater dispatch={dispatch} />);
+    expect(dispatch).toHaveBeenCalledWith(actions.fetchArticle(id));
   });
 });
